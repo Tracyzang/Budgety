3c9a008ce0310aec34fa90d71fedbc75a392bfa6
Zhiqiu Zang <40729720+Tracyzang@users.noreply.github.com>
2019-01-13 16:46:41 -0800

    add new items to UI

--- app.js
+++ app.js
@@ -57,23 +57,63 @@ var budgetController = (function() {
 
 //UI Controller
 var UIController = (function() {
-  var DOMstings = {
+  var DOMstrings = {
     inputType: ".add__type",
     inputDescription: ".add__description",
     inputValue: ".add__value",
-    inputBtn: ".add__btn"
+    inputBtn: ".add__btn",
+    incomeContainer: ".income__list",
+    expensesContainer: ".expenses__list"
   };
   return {
     getinput: function() {
       return {
-        type: document.querySelector(DOMstings.inputType).value, //will be either inc or exp
-        description: document.querySelector(DOMstings.inputDescription).value,
-        value: document.querySelector(DOMstings.inputValue).value
+        type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
       };
     },
 
+    addListItem: function(obj, type) {
+      var html, newHtml, element;
+
+      //Create HTML string with placeholder text
+      if (type === "inc") {
+        element = DOMstrings.incomeContainer;
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === "exp") {
+        element = DOMstrings.expensesContainer;
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      //Replace the placeholder text with some actual data
+      newHtml = html.replace("%id%", obj.id);
+      newHtml = newHtml.replace("%description%", obj.description);
+      newHtml = newHtml.replace("%value%", obj.value);
+
+      //Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
+    },
+
+    clearFields: function() {
+      var fields, fieldsArr;
+      fields = document.querySelectorAll(
+        DOMstrings.inputDescription + "," + DOMstrings.inputValue
+      );
+
+      var fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+
+      fieldsArr[0].focus();
+    },
+
     getDOMstrings: function() {
-      return DOMstings;
+      return DOMstrings;
     }
   };
 })();
@@ -96,10 +136,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     var input, newItem;
     //1. Get the field input data
     input = UICtrl.getinput();
-    console.log(input);
     //2.Add the item to the budget controller
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     //3.Add the item to the UI
+
+    UICtrl.addListItem(newItem, input.type);
+
+    //4. Clear the fieldsArr
+
+    UICtrl.clearFields();
     //4.Calculate the budget
     //5.Display the budget
   };
