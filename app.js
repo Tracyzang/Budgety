f93165742de7a4918aca7c4eb22e8ce710a929bc
Zhiqiu Zang <40729720+Tracyzang@users.noreply.github.com>
2019-01-15 12:06:02 -0800

    adding delete to the UI

--- app.js
+++ app.js
@@ -60,6 +60,17 @@ var budgetController = (function() {
       return newItem;
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+      index = ids.indexOf(id);
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
+
     calculateBudget: function() {
       //calculate total income and expense
       calculateTotal("exp");
@@ -137,6 +148,11 @@ var UIController = (function() {
 
       //Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
+    },
+
+    deleteListItem: function(selectorID) {
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
     },
 
     clearFields: function() {
@@ -229,11 +245,14 @@ var controller = (function(budgetCtrl, UICtrl) {
       //inc-1
       splitID = itemID.split("-");
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]);
 
       //1. delete the item from the data structure
+      budgetCtrl.deleteItem(type, ID);
       //2. delete the item from the UI
+      UICtrl.deleteListItem(itemID);
       //3. Update and show the new budget
+      updateBudget();
     }
   };
 
